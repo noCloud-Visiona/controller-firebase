1262fec0b6833d8175de074be1561172b5dceacc
Jonatas Mathias <jonatasmathiasdallo@gmail.com>
2024-10-25 15:08:12 -0300

    Hotfix na função de salvar o json e historico, onde é retornado o geometry e user_geometry igual o front manda.

--- controller/firebase_gets/historico_by_id.py
+++ controller/firebase_gets/historico_by_id.py
@@ -1,13 +1,28 @@
 from firebase import db
 from flask import jsonify
 
+def transformar_coordenadas(coordenadas):
+    # Ordena as coordenadas pela chave (coordinate1, coordinate2, ...)
+    ordered_keys = sorted(coordenadas.keys())
+    # Converte para uma lista de listas em ordem
+    return [[coordenadas[key]["longitude"], coordenadas[key]["latitude"]] for key in ordered_keys]
+
 def get_historico(id_usuario):
     collection_ref = db.collection("historico_imagens_ia")
-
-    # Recuperando os documentos que têm o campo "id_usuario" igual ao valor passado
     documentos = collection_ref.where("id_usuario", "==", id_usuario).stream()
 
-    # Criando uma lista para armazenar os dados
-    historico = [doc.to_dict() for doc in documentos]
+    historico = []
+    for doc in documentos:
+        item = doc.to_dict()
+
+        # Transforma "geometry" se existir
+        if "geometry" in item and "coordinates" in item["geometry"]:
+            item["geometry"]["coordinates"] = [transformar_coordenadas(item["geometry"]["coordinates"])]
+
+        # Transforma "user_geometry" se existir
+        if "user_geometry" in item and "coordinates" in item["user_geometry"]:
+            item["user_geometry"]["coordinates"] = [transformar_coordenadas(item["user_geometry"]["coordinates"])]
+
+        historico.append(item)
 
     return jsonify(historico)
--- controller/firebase_post/post_json_front.py
+++ controller/firebase_post/post_json_front.py
@@ -106,8 +106,73 @@ def post_json_front():
         }
     }
 
+    jsonFinalResposta = {
+        "type": json_data.get('type', None),
+        "id": id_imagem_final,
+        "id_usuario": json_data['identificacao_ia'].get('id_usuario', None),
+        "collection": json_data.get('collection', None),
+        "stac_version": json_data.get('stac_version', None),
+        "stac_extensions": json_data.get('stac_extensions', []),  
+        "data": json_data['identificacao_ia'].get('data', None),
+        "hora": json_data['identificacao_ia'].get('hora', None),
+        "geometry": json_data["geometry"],
+        "links": [
+            {
+                "href": link.get('href'),
+                "rel": link.get('rel')
+            } for link in json_data.get('links', [])  
+        ],
+        "bbox": json_data.get('bbox', None),
+        "assets": {
+            "EVI": {
+                "href": json_data['assets']['EVI'].get('href', None),
+                "type": json_data['assets']['EVI'].get('type', None),
+                "roles": json_data['assets']['EVI'].get('roles', []),  
+                "created": json_data['assets']['EVI'].get('created', None),
+                "updated": json_data['assets']['EVI'].get('updated', None),
+                "bdc:size": json_data['assets']['EVI'].get('bdc:size', None),
+                "bdc:chunk_size": json_data['assets']['EVI'].get('bdc:chunk_size', None),
+                "bdc:raster_size": json_data['assets']['EVI'].get('bdc:raster_size', None),
+                "checksum:multihash": json_data['assets']['EVI'].get('checksum:multihash', None)
+            },
+            "thumbnail": {
+                "href": json_data['assets']['thumbnail'].get('href', None),
+                "type": json_data['assets']['thumbnail'].get('type', None),
+                "roles": json_data['assets']['thumbnail'].get('roles', []),  
+                "created": json_data['assets']['thumbnail'].get('created', None),
+                "updated": json_data['assets']['thumbnail'].get('updated', None),
+                "bdc:size": json_data['assets']['thumbnail'].get('bdc:size', None),
+                "checksum:multihash": json_data['assets']['thumbnail'].get('checksum:multihash', None)
+            }
+        },
+        "properties": {
+            "datetime": json_data['properties'].get('datetime', None),
+            "start_datetime": json_data['properties'].get('start_datetime', None),
+            "end_datetime": json_data['properties'].get('end_datetime', None),
+            "created": json_data['properties'].get('created', None),
+            "updated": json_data['properties'].get('updated', None),
+            "eo:cloud_cover": json_data['properties'].get('eo:cloud_cover', None)
+        },
+        "user_geometry": json_data["user_geometry"],
+        "identificacao_ia": {
+            "id": id_imagem_final,
+            "area_visivel_mapa": json_data['identificacao_ia'].get('area_visivel_mapa', None),
+            "percentual_nuvem": json_data['identificacao_ia'].get('percentual_nuvem', None),
+            "percentual_sombra_nuvem": None,
+            "img_original_png": json_data['identificacao_ia'].get('img_original_png', None),
+            "img_original_tiff": json_data['identificacao_ia'].get('img_original_tiff', None),
+            "img_tratada": json_data['identificacao_ia'].get('img_tratada', None),
+            "mask_nuvem": json_data['identificacao_ia'].get('mask_nuvem', None),
+            "mask_sombra": None,
+            "tiff_tratado": None,
+            "resolucao_imagem_png": json_data['identificacao_ia'].get('resolucao_imagem_png', None),
+            "resolucao_imagem_tiff": None,
+            "bbox": json_data['identificacao_ia'].get('bbox', None)
+        }
+    }
+
     # Adiciona o JSON final à coleção no Firestore
     collection_ref.add(jsonFinal)
 
     # Retorna o JSON final como resposta
-    return jsonify(jsonFinal), 201
+    return jsonify(jsonFinalResposta), 201
