d8d6e6a80bd63c3000020ab83761e6a8d67f7f2b
Jonas Ribeiro <jonas.ribeiro01@fatec.sp.gov.br>
2024-11-10 22:03:26 -0300

    trocando porta pra 3004

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def exibe_imagem_json():
         return jsonify({"message": mensagem})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=3005)
+    app.run(debug=True, host="0.0.0.0", port=3004)
